fe94e9ac346e80eb039cd848ef6ee7a8d2387f91
Shijing Liu <brendaliu0628@gmail.com>
2020-10-12 19:33:12 -0500

    "post " frontend has issue

---
+++ index.js
@@ -0,0 +1,100 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+let notes = [
+ {
+  id: 1,
+  content: "HTML is easy",
+  date: "2019-05-30T17:30:31.098Z",
+  important: true
+ },
+ {
+  id: 2,
+  content: "Browser can execute only Javascript",
+  date: "2019-05-30T18:39:34.091Z",
+  important: false
+ },
+ {
+  id: 3,
+  content: "GET and POST are the most important methods of HTTP protocol",
+  date: "2019-05-30T19:20:14.298Z",
+  important: true
+ },
+ {
+  id: 4,
+  content: "new item",
+  date: "2020-05-30T19:20:14.298Z",
+  important: true
+ }
+]
+
+app.use(cors());
+
+// generate the max number of the newest Id
+const generateId = () => {
+ const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+ return maxId + 1;
+
+}
+
+//Route
+app.get('/', cors(), (req, res) => {
+ res.send('<h1>Hi I am brenda</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+ res.json(notes)
+})
+// Find one item
+app.get('/api/notes/:id', (req, res) => {
+ // This ID need to convert to a number
+ const id = Number(req.params.id);
+ const note = notes.find(note => note.id === id);
+ if (note) {
+  res.json(note);
+ }
+ else {
+  res.status(404).end();
+ }
+})
+
+// Delete one item
+app.delete('/api/notes/:id', (req, res) => {
+ const id = Number(req.params.id);
+ notes = notes.filter(note => note.id !== id);
+ res.status(204).end();
+})
+
+app.post('/api/notes', (req, res) => {
+ const body = req.body;
+ if (!body.content) {
+  return res.status(400).json({
+   error: 'content missing'
+  })
+ }
+
+ const note = {
+  content: body.content,
+  important: body.important || false,
+  date: new Date(),
+  id: generateId(),
+ }
+
+ notes = notes.concat(note)
+
+ res.json(note)
+})
+
+//  create web server without express
+// const app = http.createServer((req, res) => {
+//  res.writeHead(200, { 'Constent-Type': "text/plain" })
+//  res.end(JSON.stringify(notes))
+// })
+
+
+//Listen port 3001
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
